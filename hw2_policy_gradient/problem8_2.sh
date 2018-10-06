ff8b5ac013637ddee00733d3d6fbe2a5cfabef4e
Russzheng <marshzhengzhe@icloud.com>
2018-10-05 23:47:08 -0700

    delete redundant files

--- hw2_policy_gradient/problem8_2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eux
-
-python train_pg_f18.py HalfCheetah-v2 -ep 150 --discount 0.95 -n 100 -e 3 -l 2 -s 32 -b 50000 -lr 0.02 --exp_name hc_b50000_r0.02
-python train_pg_f18.py HalfCheetah-v2 -ep 150 --discount 0.95 -n 100 -e 3 -l 2 -s 32 -b 50000 -lr 0.02 -rtg --exp_name hc_b50000_r0.02_rtg
-python train_pg_f18.py HalfCheetah-v2 -ep 150 --discount 0.95 -n 100 -e 3 -l 2 -s 32 -b 50000 -lr 0.02 --nn_baseline --exp_name hc_b50000_r0.02_no_rtg_baseline
-python train_pg_f18.py HalfCheetah-v2 -ep 150 --discount 0.95 -n 100 -e 3 -l 2 -s 32 -b 50000 -lr 0.02 -rtg --nn_baseline --exp_name hc_b50000_r0.02_rtg_baseline
-
-python plot.py data/hc_b50000_r0.02*
